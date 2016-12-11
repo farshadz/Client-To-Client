93b5ed9f4083571dda01f088c1d06b58c4969fc1
Farshad zamani <zamani.farshad@gmail.com>
2016-12-11 12:41:57 +0100

    Commenting done

---
+++ Client-To-Client/communicationTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Controls;
+
+namespace communicationTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
